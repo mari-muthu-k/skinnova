970fcd087aae24a9f165bba1cdb8c0567c45a274
Mari muthu <73837574+mari-muthu-k@users.noreply.github.com>
2025-12-06 02:38:53 +0530

    Refactor FastAPI setup: add CORS middleware, enhance exception handling, and update application structure

--- api/main.py
+++ api/main.py
@@ -1,20 +1,29 @@
-from fastapi import FastAPI
-from routers import llm, health
+import os
+from fastapi import FastAPI, Request
+from routers import health, llm
+from utils.exception_handler import ExceptionBase 
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from webhooks import datadog_incident
 
+app = FastAPI(title="SkinNova LLM Backend with Datadog Observability", version="1.0.0",openapi_url="",root_path="/api/v1")
 
-app = FastAPI(title="LLM Backend with Datadog Observability")
+#Todo : add startup and shutdown events for db connections etc
 
-app.include_router(llm.router, prefix="/v1/llm")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("CORS_ALLOW_ORIGINS").split(",")],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["Content-Type","Content-Length"],
+)
+
+app.include_router(llm.router, prefix="/llm")
 app.include_router(health.router, prefix="/health")
 app.include_router(datadog_incident.router, prefix="/webhooks/datadog")
 
-@app.get("/")
-def root():
-    return {"status": "running"}
-
-@app.post("/send-test-metric")
-def send_test_metric():
-    from services.datadog_service import send_metric
-    send_metric("llm_backend.test.metric", 1)
-    return {"status": "metric sent"}
+@app.exception_handler(ExceptionBase)
+async def exception_handler(request: Request, exc: ExceptionBase):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message":exc.message},
+    )
