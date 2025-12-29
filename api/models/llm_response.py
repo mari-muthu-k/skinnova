09bd8fed5394f07c9ce62d3e9624414ca9be7575
Mari muthu <73837574+mari-muthu-k@users.noreply.github.com>
2025-12-29 15:48:10 +0530

    pydantic model error fixed

--- api/models/llm_response.py
+++ api/models/llm_response.py
@@ -3,12 +3,12 @@ from pydantic import BaseModel, Field
 
 
 class DataModel(BaseModel):
-    Profile :  User | None = Field(..., alias="profile")
+    Profile :  User | None = Field(alias="profile",default=None)
     Response : str = Field(..., alias="response")
-    MorningRoutine : list | None = Field(... , alias="morning_routine")
-    EveningRoutine : list | None = Field(... , alias="evening_routine")
-    NightRoutine : list | None = Field(... , alias="night_routine")
-    UsageInstructions : str | None = Field(... , alias="usage_instructions")
+    MorningRoutine : list | None = Field(alias="morning_routine",default=None)
+    EveningRoutine : list | None = Field(alias="evening_routine",default=None)
+    NightRoutine : list | None = Field(alias="night_routine",default=None)
+    UsageInstructions : str | None = Field(alias="usage_instructions",default=None)
 
 class LLMResponse(BaseModel):
     Type: str = Field(..., alias="type", description="type of response") # values : "chat", "routine" or "error"
