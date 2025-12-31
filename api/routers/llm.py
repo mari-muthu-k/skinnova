869aab50339d656ab560218ca05a02058e785df7
Mari muthu <73837574+mari-muthu-k@users.noreply.github.com>
2025-12-31 16:54:47 +0530

    llm null response for routines fixed

--- api/routers/llm.py
+++ api/routers/llm.py
@@ -7,4 +7,6 @@ router = APIRouter()
 @router.post("/chat")
 async def chat_llm(input: dict):
     llm_chat_res : LLMResponse = await llm_chat(input)
+    if llm_chat_res.Type == "routine":
+        return {"result": llm_chat_res}
     return {"result": llm_chat_res.Data.Response}
